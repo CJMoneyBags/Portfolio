63f4e4349ee1581a76b093c2cb9e00fce515e605
CJMoneyBags <49886936+CJMoneyBags@users.noreply.github.com>
2022-04-23 20:08:03 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,27 +11,14 @@ const links = [
 
 function makeList()
 {
-  var listContainer = document.createElement('div');
-  var listElement = document.createElement('ol');
-  var numListItems = links.length;
-  var listItem;
-  var i;
+ var listItems = document.createElement('ol');
+ for (var i in links){
+   var anchor = document.createElement('a');
+   anchor.href = links[i].url;
+   anchor.innerText = links[i].label;
 
-  document.getElementsByTagName('body')[0].appendChild(listContainer);
-  listContainer.appendChild(listElement);
-
-  for(i = 0; i < numListItems; i++)
-  {
-    // create line items, populate them, and add list item to list element
-    listItem = document.createElement('li');
-    listItem.innerHTML = links[i];
-    listElement.appendChild(listItem);
-  }
+   var elem = document.createElement("li");
+   elem.appendChild(anchor);
+   list.appendChild(elem);
+ }
 }
-
-var li = document.createElement('li'),
-        anchor = document.createElement('a');
-        anchor.href = links.url;
-        anchor.text = links.label;
-        li.appendChild(anchor);
-        ul.appendChild(li);
