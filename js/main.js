35c24440d564ccc47c1f5353a3cbcc6906b2f5b7
CJMoneyBags <49886936+CJMoneyBags@users.noreply.github.com>
2022-04-23 22:54:40 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,6 +8,18 @@ function makeList()
       url: "Week01/index.html"
     },
 
+    {
+      label: "Week2 notes",
+      url: "Week02/index.html"
+    },
+    {
+      label: "Week3 notes",
+      url: "Week03/index.html"
+    },
+    {
+      label: "Week4 notes",
+      url: "Week04/index.html"
+    },
   ]
 
 var lists = document.getElementById("WeekList");
